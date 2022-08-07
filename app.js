94719978ae97199c82b792b673bd3e4ad184778d
Linika <lidya-novikova.95@yandex.ru>
2022-08-07 23:35:11 +0300

    Add checkbox and delete button

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
+//Получение элементов HTML
 const task=document.querySelector(".td_input");
-const list_container=document.querySelector(".td_list");
+let list_container=document.querySelector(".td_list");
 
+//Переменные и массив - контейнер для списка
+let list_item,checkbox,list_item_container,delete_btn
+let documentFragment=document.createDocumentFragment()
 let to_do_list=[];
 
 
-function addToDo(text){
+function addToDo(text){     //Добавление задания в список
     if (task.value.trim().length == 0) return
     else{
         to_do_list.push(task.value);
-        console.log(to_do_list)
-        let x=document.createElement('div')
-        x.classList.add('td_list_item')
-        list_container.appendChild(x)
-        x.innerText=(to_do_list.length)+".  "+to_do_list[to_do_list.length-1]
+        addElementToList()
+        list_item.innerHTML=to_do_list[to_do_list.length-1]
         task.value = ""
         task.focus()
     }
 }
-function setFocus(){
+function addElementToList(){        //Создание элемента списка с checkbox и кнопкой удаления
+    list_item_container=document.createElement('li')
+    list_item=document.createElement('span')
+    checkbox=document.createElement('input')
+    checkbox.type="checkbox"
+    delete_btn=document.createElement('button')
+    delete_btn.type="submit"
+    list_item_container.classList.add('td_list_item')
+    checkbox.classList.add('td_list_item_checkbox')
+    delete_btn.classList.add('delete_btn')
+    documentFragment.appendChild(list_item_container)
+    list_item_container.appendChild(list_item)
+    list_item_container.appendChild(checkbox)
+    list_item_container.appendChild(delete_btn)
+    list_container.appendChild(documentFragment)
 }
-// task.addEventListener("keyup",function event(){
-//     event.preventDefault();
-//     if (event.keyCode ===13){
-//         addToDo();
-//     }
-// })
+
+// checkbox.onchange= function(){
+//     if(checkbox.checked)
+//         list_item_container.classList.add('td_list_item_done')
+// }
